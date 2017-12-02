e6f66e0bb0ad36a72d7694ef9200acb324d722f1
wqferr <wqferr@gmail.com>
2017-12-02 19:38:36 +0000

    Initial version of Board-Actor architecture

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+
+	public enum Action {
+		MOVE_U,
+		MOVE_D,
+		MOVE_L,
+		MOVE_R,
+		ATTACK
+	}
+
+	public int initR, initC;
+	public int r { get; private set; }
+	public int c { get; private set; }
+
+    private List<Action> plan;
+	private IEnumerator<Action> actions;
+	private Board board;
+
+	public void Spawn(int initR, int initC) {
+		plan = new List<Action>();
+		board = GameObject.FindObjectOfType<Board>();
+
+		this.initR = initR;
+		this.initC = initC;
+	}
+
+	public void Restart() {
+		r = initR;
+		c = initC;
+		actions = plan.GetEnumerator();
+	}
+
+    public void AddAction(Action a) {
+		plan.Add(a);
+	}
+
+	public void ClearActions() {
+		plan.Clear();
+	}
+	
+    public bool NextAction() {
+		if (!actions.MoveNext()) {
+			return false;
+		}
+		// TODO iTween
+		switch (actions.Current) {
+			case Action.MOVE_U:
+				if (board.Move(r, c, r-1, c)) {
+					r--;
+				}
+				break;
+			case Action.MOVE_D:
+				if (board.Move(r, c, r+1, c)) {
+					r++;
+				}
+				break;
+			case Action.MOVE_L:
+				if (board.Move(r, c, r, c-1)) {
+					c--;
+				}
+				break;
+			case Action.MOVE_R:
+				if (board.Move(r, c, r, c+1)) {
+					c++;
+				}
+				break;
+			case Action.ATTACK:
+				// TODO attack
+				break;
+		}
+		return true;
+    }
+
+}
---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Board : MonoBehaviour {
+
+	public const float CELL_SIZE = 1.0f;
+	public const int GRID_ROWS = 5;
+	public const int GRID_COLS = 8;
+
+	private GameObject[,] cells;
+
+	void Awake() {
+		cells = new GameObject[GRID_ROWS, GRID_COLS];
+	}
+
+	public GameObject Get(int r, int c) {
+		return cells[r, c];
+	}
+
+	public void Set(int r, int c, GameObject obj) {
+		cells[r, c] = obj;
+	}
+
+	public bool Move(int r0, int c0, int r1, int c1) {
+		if (cells[r1, c1] != null) {
+			return false;
+		}
+		cells[r1, c1] = cells[r0, c0];
+		cells[r0, c0] = null;
+		return true;
+	}
+
+}
