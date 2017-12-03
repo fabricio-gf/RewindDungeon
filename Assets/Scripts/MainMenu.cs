909b5515c5f64665ea5e9083594148796218b0d5
Fabrício Guedes Faria <fabricio-gf@hotmail.com>
2017-12-03 16:27:39 -0200

    Added edited MainMenu script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangePanel(string str)
+    {
+        //changes focus to another panel in this scene
+    }
+
+    public void OpenPanel(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+
+    public void ClosePanel(GameObject obj)
+    {
+		obj.SetActive(false);
+    }
+
+    public void Teste()
+    {
+        print("teste");
+    }
+}
