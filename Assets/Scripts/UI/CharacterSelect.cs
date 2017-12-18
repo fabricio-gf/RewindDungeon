7f71046c1236928d9aad9b493175c0245c45bdef
wqferr <wqferr@gmail.com>
2017-12-18 18:52:10 +0000

    Convert levels to ScriptableObjects

--- Assets/Scripts/UI/CharacterSelect.cs
+++ Assets/Scripts/UI/CharacterSelect.cs
@@ -16,7 +16,7 @@ public class CharacterSelect : MonoBehaviour {
 
         for(int i = 0; i < AvailableCharacters.Count; i++)
         {
-            obj =  Instantiate(ButtonPrefab, transform);
+            obj = Instantiate(ButtonPrefab, transform);
             obj.transform.Find("Text").GetComponent<Text>().text = AvailableCharacters[i].GetComponent<Actor>().info.Preview.ToString();
             obj.GetComponent<CharacterButton>().index = i;
             obj.GetComponent<CharacterButton>().CharPrefabs = AvailableCharacters;
