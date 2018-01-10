6fd1a0723055af174814d09dae502facd8ffe12c
wqferr <wqferr@gmail.com>
2018-01-10 14:56:21 +0000

    Remove references to UnityEditor when possible

--- Assets/ScriptableObjects/Level.cs
+++ Assets/ScriptableObjects/Level.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 [CreateAssetMenu(fileName = "Level", menuName = "Level", order = 1)]
 public class Level : ScriptableObject {
